f9e357f552a2ce1e8d40f2f2cbf96ecb71e5bf72
bala-amypo <bala@amypo.in>
2025-12-26 05:18:11 +0000

    User commit

--- src/test/java/com/example/demo/TestResultListener.java
+++ src/test/java/com/example/demo/TestResultListener.java
@@ -1,22 +1,22 @@
 package com.example.demo;
-
-import org.testng.ITestContext;
+ 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
+ 
 public class TestResultListener implements ITestListener {
-    @Override
-    public void onTestStart(ITestResult result) {}
-
+ 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("Passed: " + result.getName());
+        System.out.println(result.getMethod().getMethodName() + " - PASS");
     }
-
+ 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("Failed: " + result.getName());
+        System.out.println(result.getMethod().getMethodName() + " - FAIL");
     }
-    
-    // Add other empty overrides for ITestListener methods if needed
+ 
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println(result.getMethod().getMethodName() + " - SKIP");
+    }
 }
