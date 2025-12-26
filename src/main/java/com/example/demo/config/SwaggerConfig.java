a445b8fea9bb85dd8e0dc5d097213682a5ba92f5
bala-amypo <bala@amypo.in>
2025-12-26 08:43:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,22 +2,17 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Claims Management API")
+                        .title("Insurance Fraud API")
                         .version("1.0")
-                        .description("API for managing insurance claims and fraud detection"))
-                .servers(List.of(new Server().url("http://localhost:8080").description("Local Server")));
+                        .description("API Documentation for Fraud Pattern Detector"));
     }
 }
